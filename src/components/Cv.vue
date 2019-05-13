c2d488bd7655fcc26d10010f0e5c87b360403957
Sofiane Lakhneche <lakhneche.sofiane@gmail.com>
2019-05-14 00:02:25 +0200

    add cv

---
+++ src/components/Cv.vue
@@ -0,0 +1,279 @@
+<template>
+  <div class="hello">
+  <!--  <div id="demo">
+      <button v-on:click="show = !show">
+        Permuter
+      </button>
+    </div> -->
+    <!--<h1>{{ msg }}</h1>-->
+<header>
+  <div class="container">
+
+    <h1>  
+    Sofiane Lakhneche
+    </h1>  
+    
+  </div>
+</header>
+
+<div class="container">
+  <h2>Experience</h2>
+  <div class="rule"></div>
+   <div class="item">
+    <h3>Stage - Developpeur Réalité Augmentée &nbsp;&nbsp;</h3>
+    <h4>Evolutis</h4>
+    <h5>2018. </h5>
+  </div>
+  <div class="item">
+    <h3>Stage - Developpeur web et mobie&nbsp;&nbsp;</h3>
+    <h4>Inaya Medias</h4>
+    <h5>2016—2017. Courbevoie.</h5>
+  </div>
+  <div class="item">
+    <h3>Application ANDROID : NUANCE&nbsp;&nbsp;</h3>
+    <h4>Projet universitaire</h4>
+    <h5>2016. Université Paris 13</h5>
+  </div>
+  
+  <h2>Formations</h2>
+  <div class="rule"></div>
+  <div class="item">
+    <h3>Licence pro&nbsp;&nbsp;</h3>
+    <h4>Developpeur web et mobile</h4>
+    <h5>2017 - 2018. Université Paris 13</h5>
+  </div>
+   <div class="item">
+    <h3>DUT&nbsp;&nbsp;</h3>
+    <h4>Metiers du multimedia et de l'internet</h4>
+    <h5>Université paris 13</h5>
+  </div>
+   <div class="item">
+    <h3>Baccalauréat&nbsp;&nbsp;</h3>
+    <h4>Communication visuelle</h4>
+    <h5>Lycee Alfred costes</h5>
+  </div>
+  <!-- <div class="item">
+    <h3>Bachelor <span class="of">of</span> Fine Arts&nbsp;&nbsp;</h3>
+    <h4>Graphic Design</h4>
+    <h5>Grand Valley State University.</h5>
+  </div> -->
+  
+  <h2>Compétences</h2>
+  <div class="rule"></div>
+  <div class="item">
+    <h3>Design&nbsp;&nbsp;</h3>
+    <h4>
+      <ul>
+        <li>Sketch 3</li>
+        <li>Photoshop</li>
+        <li>Illustrator</li>
+        <li>InDesign</li>
+      </ul>
+    </h4>
+    <h3>Code&nbsp;&nbsp;</h3>
+    <h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+    </h4>
+  </div>
+  
+ <!-- <h2>Involvement</h2>
+  <div class="rule"></div>
+  <div class="item">
+    <h3>AIGA&nbsp;&nbsp;</h3>
+    <h4>Chicago / West Michigan</h4>
+    <h5>Member.</h5>
+  </div>
+  <div class="item">
+    <h3>Frederik Meijer Honors College&nbsp;&nbsp;</h3>
+    <h4>GVSU</h4>
+    <h5>Mentor Council Member. Freshmen Mentor.</h5>
+  </div>
+  <div class="item">
+    <h3>GV GrooVe! A Cappella&nbsp;&nbsp;</h3>
+    <h4>GVSU</h4>
+    <h5>Music Director.</h5>
+  </div> -->
+</div>
+
+<footer>
+  <div class="container">
+    <h3>Contact: &nbsp;&nbsp;</h3>
+        <a href="mailto:lakhneche.sofiane@gmail.com"><h4>lakhneche.sofiane@gmail.com</h4></a>
+  </div>
+</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data() {
+    return {
+      show: true,
+    }
+  },
+  methods: {
+    onClick() {
+      console.log('toz', this.name, this.bgcolor);
+      this.showModal(this.name, this.bgcolor);
+    }
+  },
+  props: {
+    name: String,
+    bgcolor: String,
+    backgroundImage: String,
+    showModal: Function
+  }
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+@import url('https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i');
+
+h1, h2, h3, h4 {
+  margin: 0;
+  padding: 0;
+  font-family: "PathwayGothicOne", serif;
+  font-size: 16px;
+  font-weight: 300;
+  color: #BFBFBF;
+  line-height: 50px;
+}
+
+header {
+  color: #FFF;
+  background: #87BBD8;
+  font-weight: 700;
+  font-size: 24px;
+}
+
+.container {
+  padding: 20px;
+}
+
+.item {
+  margin: 20px 0;
+}
+
+h2 {
+  color: #87BBD8;
+  font-weight: 700;
+  border-bottom: 5px solid #87BBD8;
+  display: inline-block;
+  line-height: 40px;
+  margin-top: 28px;
+}
+
+.rule {
+  box-sizing: border-box;
+  height: 1px;
+  background: #87BBD8;
+}
+
+h3 {
+  color: #979797;
+  font-weight: 700;
+  margin-top: 20px;
+}
+
+.of {
+  font-weight: 300;
+  font-style: italic;
+}
+
+h4 {
+
+}
+
+h4 ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+h3, h4 {
+  line-height: 28px;
+}
+
+h5 {
+  font-size: 10px;
+  color: #87BBD8;
+  margin-top: 0;
+  line-height: 32px;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+}
+
+footer {
+  background: #87BBD8;
+  color: #FFF;
+  margin-top: 60px;
+}
+
+footer h3 {
+  color: #FFF;
+}
+
+footer h4 {
+  color: #EEE;
+}
+
+@media (min-width: 645px) {
+  
+  body, h1, h2, h3, h4 {
+    font-size: 24px;
+  }
+  
+  header {
+    padding: 30px 0;
+    margin-bottom: 16px;
+    font-size: 32px;
+  }
+  
+  .container {
+    width: 85%;
+    margin: 0 auto;
+  }
+  
+  h5 {
+    font-size: 12px;
+  }
+  
+  h3, h4 {
+    display: inline-block;
+    vertical-align: top;
+    margin-top: 20px;
+  }
+    
+  h4 ul {
+    margin-right: 48px;
+  }
+  
+  h4 li {
+    padding: 0 0 12px 0;
+  }
+  
+  footer {
+    padding: 30px 0 40px;
+  }
+  
+}
+
+@media (min-width: 820px) {
+  
+  .container {
+    width: 820px;
+    margin: 0 auto;
+  }
+  
+}
+
+</style>
---
+++ src/components/PopIn.vue
@@ -0,0 +1,105 @@
+<script>
+  export default {
+    name: 'modal',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header
+          class="modal-header"
+          id="modalTitle"
+        >
+          <slot name="header">
+            This is the default tile!
+
+            <button
+              type="button"
+              class="btn-close"
+              @click="close"
+              aria-label="Close modal"
+            >
+              x
+            </button>
+          </slot>
+        </header>
+        <section
+          class="modal-body"
+          id="modalDescription"
+        >
+          <slot name="body">
+            I'm the default body!
+          </slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            I'm the default footer!
+
+            <button
+              type="button"
+              class="btn-green"
+              @click="close"
+              aria-label="Close modal"
+            >
+              Close me!
+            </button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+<style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #111629;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .modal {
+    background: rgb(135, 186, 215);
+    /*box-shadow: 2px 2px 20px 1px;*/
+    overflow-x: auto;
+    /*display: flex;
+    flex-direction: column;*/
+    width: 1000px;
+    height: 600px;
+    font-family: "Chivo";
+    font-size: 130%;
+  }
+  .modal-header,
+  .modal-footer {
+    padding: 15px;
+    display: flex;
+  }
+  .modal-header {
+    border-bottom: 1px solid #eeeeee;
+    color: rgb(235, 247, 245);
+    justify-content: space-between;
+  }
+  .btn-close {
+    border: none;
+    font-size: 40px;
+    padding: 20px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #111629;
+    background: transparent;
+    font-size: 130%;
+  }
+</style>
